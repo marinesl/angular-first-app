fb906a1d2021a7145b41f8ec4399bf68e1bdec7e
Marine Lancelin <lancelinmarine@gmail.com>
2024-03-18 16:15:29 +0100

    :tada: First and last commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# angular-first-app
+# My first Angular App
+
+Tutorial: https://angular.io/tutorial/first-app
+
+## How to use it
+1. Install the project
+2. Check your node version: ```npm --version```
+3. Install Node modules: ```npm install```
+4. Run server: ```ng serve```
+5. Run Json server (in other terminal): ```json-server --watch db.json```
---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+import { RouterLink, RouterOutlet } from "@angular/router";
+import { HousingLocation } from "../housinglocation";
+
+@Component({
+	selector: 'app-housing-location',
+	standalone: true,
+	imports: [
+		RouterLink,
+		RouterOutlet,
+	],
+	template: `
+		<section class="listing">
+			<img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{ housingLocation.name }}">
+			<h2 class="listing-heading">{{ housingLocation.name }}</h2>
+			<p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+			<a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+		</section>
+  	`,
+	styleUrl: './housing-location.component.css'
+})
+
+export class HousingLocationComponent {
+	@Input() housingLocation!: HousingLocation; // "!" means non-null assertion operator => the value of this property won't be null or undefined 
+}
